b2c5f50fa4bde2bf333f247057119d8a0d4c174b
zakaria-ahmd20 <94662829+zakaria-ahmd20@users.noreply.github.com>
2022-06-25 15:09:27 -0400

    Create CSV_creation.py
    creates the CSV file with the AD users data , once finished we will provide this file to the PowerShell script to import in AD.

---
+++ CSV_creation.py
@@ -0,0 +1,54 @@
+import csv
+
+def header():
+    headerList = ['firstname', 'lastname', 'office', 'phone','title','accountname']
+
+    # open CSV file and assign header
+    with open("students.csv", 'w') as file:
+        dd = csv.DictWriter(file, delimiter=',',
+                            fieldnames=headerList)
+        dd.writeheader()
+#import paramiko
+def add_user():
+    infile = open("students.csv", "a")
+    first_last_name = str((input('enter your first_last_name:')))
+    last_name = str(input('enter your last_name:'))
+    phone = str(input('what is your phone number:'))
+    office = str(input('which office are you in:'))
+    title = input('what is your title:')
+    account_name = input('what is your account name:')
+
+    data1 = f"\n{first_last_name},{last_name},{office},{phone},{title},{account_name}"
+    data = data1.strip() # strips white space
+    infile.write(data +"\n") # appends to new line
+    print('record added succesfully !')
+    infile.close()
+
+
+def main():
+
+    print('Welcome to CSV creation')
+    print('********************************************************'
+        ' **  \n'
+        ' **	Please choose one of the options below:\n'
+        ' **	[a] to add new student record\n'
+        ' **	[h] to initialze headers\n'  
+        ' **	[q] to quit \n'
+        ' ********************************************************\n')
+    choice = input('pick:')
+    while True:
+        if choice == "a":
+            add_user()
+            break
+        elif choice == "h":
+            header()
+            break
+        elif  choice == "q":
+            break
+
+        else: print('Invalid choice!')
+
+
+
+
+main()# calls all the code
