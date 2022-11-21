24dac0a52fe59133831fd6193588a70832e61e82
zakaria-ahmd20 <94662829+zakaria-ahmd20@users.noreply.github.com>
2022-11-20 20:49:24 -0500

    Update CSV_creation.py

--- CSV_creation.py
+++ CSV_creation.py
@@ -1,26 +1,24 @@
 # Zakaria Ahmed
-import pandas as pd
 import csv
 
 def header():
-    headerList = ['firstname', 'lastname', 'username', 'email','StreetAddress','City','ZipCode','State','Country']
+    headerList = ['username','passcode']
 
     # open CSV file and assign header
-    with open("students.csv", 'w') as file:
+    with open("students.csv", 'w',newline='') as file:
         dd = csv.DictWriter(file, delimiter=',',
                             fieldnames=headerList)
         dd.writeheader()
-#import paramiko
+
+
+
 def add_user():
     infile = open("students.csv", "a")
-    first_last_name = str((input('enter your first_last_name:')))
-    last_name = str(input('enter your last_name:'))
-    phone = str(input('what is your phone number:'))
-    office = str(input('which office are you in:'))
-    title = input('what is your title:')
-    account_name = input('what is your account name:')
+    username = str((input('enter your username:')))
+    passcode = "Adidas123_"
 
-    data1 = f"\n{first_last_name},{last_name},{office},{phone},{title},{account_name}"
+
+    data1 = f"\n{username},{passcode}"
     data = data1.strip() # strips white space
     infile.write(data +"\n") # appends to new line
     print('record added succesfully !')
