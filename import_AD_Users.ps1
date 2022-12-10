0f64183fff092f3d323c1c2ff054de6a0b6f39a4
zakaria-ahmd20 <94662829+zakaria-ahmd20@users.noreply.github.com>
2022-12-09 19:57:38 -0500

    Update import_AD_Users.ps1

--- import_AD_Users.ps1
+++ import_AD_Users.ps1
@@ -1,11 +1,9 @@
-Import-Module ActiveDirectory # imports module
-$ADUsers = Import-Csv C:\students.csv 
+Import-Module ActiveDirectory
+$ADUsers = Import-Csv C:\students.csv
 foreach ($User in $ADUsers) {
+    $Name = $User.Name
     $username = $User.username
     $password = $User.passcode
-    New-ADUser -Name $username -AccountPassword (ConvertTo-secureString $password -AsPlainText -Force) -ChangePasswordAtLogon $True
-    
-    
-    
-    
+    $Title=$User.Title
+    New-ADUser -Name $Name -SamAccountName $username -AccountPassword (ConvertTo-secureString $password -AsPlainText -Force) -Title $Title
     }
