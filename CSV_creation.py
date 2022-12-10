88a4ac90b5cf2bc1aaa89d6cbfc37d62f7a2fb38
zakaria-ahmd20 <94662829+zakaria-ahmd20@users.noreply.github.com>
2022-12-09 19:58:04 -0500

    Update CSV_creation.py

--- CSV_creation.py
+++ CSV_creation.py
@@ -1,32 +1,27 @@
 # Zakaria Ahmed
+from passwordgenerator import pwgenerator
 import csv
-
 def header():
-    headerList = ['username','passcode']
+    headerList = ['Name','username','passcode','Title',]
 
     # open CSV file and assign header
     with open("students.csv", 'w',newline='') as file:
         dd = csv.DictWriter(file, delimiter=',',
                             fieldnames=headerList)
         dd.writeheader()
-
-
-
 def add_user():
     infile = open("students.csv", "a")
+    Name = str((input('enter your full name:')))
     username = str((input('enter your username:')))
-    passcode = "Nike123_"
+    passcode = pwgenerator.generate()
+    Title = str((input('enter your job title:')))
 
-
-    data1 = f"\n{username},{passcode}"
+    data1 = f"\n{username},{passcode},{Name},{Title}"
     data = data1.strip() # strips white space
     infile.write(data +"\n") # appends to new line
     print('record added succesfully !')
     infile.close()
-
-
 def main():
-
     print('Welcome to CSV creation')
     print('********************************************************'
         ' **  \n'
@@ -47,8 +42,4 @@ def main():
             break
 
         else: print('Invalid choice!')
-
-
-
-
 main()# calls all the code
