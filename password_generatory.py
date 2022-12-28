5bad87bb4af3adb9436f5999ef584085bb90b681
zakaria-ahmd20 <94662829+zakaria-ahmd20@users.noreply.github.com>
2022-12-28 13:08:23 -0500

    Update password_generatory.py

--- password_generatory.py
+++ password_generatory.py
@@ -14,13 +14,12 @@ def password_generator():
     numbers = []
     response = requests.get(word_site) # request words 200 is good
     WORDS = response.content.splitlines() # get content from that response
-    #random_word = WORDS[a].decode("utf-8")
-    random_character = (special_characters[b])
-    numbers.clear()
-    for i in range(0,3):
-        x = (random.randint(0, 9))
-        numbers.append(x)
-    random_numbers = ''.join(map(str, numbers))
+    random_character = (special_characters[b]) # will pick a random character in the position of my list
+    numbers.clear() # clear list incase user gen a new passcode
+    for i in range(0,3): # gen a random three digit number
+        x = (random.randint(0, 9)) # between 1-10
+        numbers.append(x) # append to list
+    random_numbers = ''.join(map(str, numbers)) # join the list as one entity
     random_word = WORDS[a].decode("utf-8")
     random_word_1 = WORDS[d].decode("utf-8")
     random_character_1 = (special_characters[e])
@@ -29,7 +28,7 @@ def password_generator():
     pass_1 = f"{random_word}{random_character}{random_numbers}{random_character_1}{random_word_1}{random_character_2}{random_word_2}"
     pass_2 = f"{random_word}{random_character}{random_word_1}{random_character_1}{random_character_2}{random_word_2}{random_numbers}"
     pass_3 = f"{random_character}{random_word}{random_word_1}{random_character_1}{random_character_2}{random_word_2}{random_numbers}"
-    password_decider = (random.randint(1,4))
+    password_decider = (random.randint(1,4)) # choose a unique order of characters/numbers 
     if password_decider == 1:
         return pass_1
     elif password_decider == 2:
