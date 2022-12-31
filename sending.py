5cb25e3a0358d8d6ba94ea6cc3487912ff6b2342
zakaria-ahmd20 <94662829+zakaria-ahmd20@users.noreply.github.com>
2022-12-31 14:19:02 -0500

    Update sending.py

--- sending.py
+++ sending.py
@@ -1,25 +1,30 @@
-import  os
-import ssl
-import smtplib
-import csv
-from email.message import EmailMessage
-with open("users.csv", 'r') as file:
-  csvreader = csv.reader(file)
-  next(csvreader) # removes header as we need raw data
-  for row in csvreader: # loops through CSV file
-    username = (f"{row[0]}.{row[1]}")
-    passcode = (row[2])
-    email_reciever = (row[4])
-    email_password = os.environ.get('EMAIL_PASSWORD') # windows env variable
-    email_sender = 'youremail@yourserver.com'
-    subject = f"{username}'s crediantls"
-    body = f" Hi here is the credentails for your new onboarding username is {username},password is {passcode} " 
-    em = EmailMessage()
-    em['From'] = email_sender
-    em['To'] = email_reciever
-    em['Subject'] = subject
-    em.set_content(body)
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL('smtp.yourserver.com',465,context=context) as smtp:
-        smtp.login(email_sender,email_password)
-        smtp.sendmail(email_sender,email_reciever,em.as_string())
+def email_credentials():
+  import  os
+  import ssl
+  import smtplib
+  import csv
+  from email.message import EmailMessage
+  with open("users.csv", 'r') as file:
+    csvreader = csv.reader(file)
+    next(csvreader)
+    for row in csvreader:
+      username = (f"{row[0]}.{row[1]}")
+      passcode = (row[2])
+      email_reciever = (row[4])
+      email_password = os.environ.get('EMAIL_PASSWORD')
+      email_sender = 'hawkmoonsquad@gmail.com'
+      subject = f"{username}'s crediantls"
+      body = f" Hi here is the credentails for your new onboarding username is {username},password is {passcode}" \
+             f"User will have to change their passcode on the next login" \
+             f"This email was auto_generated please do not reply and reach out to your technical specialist for issues "
+      em = EmailMessage()
+      em['From'] = email_sender
+      em['To'] = email_reciever
+      em['Subject'] = subject
+      em.set_content(body)
+      context = ssl.create_default_context()
+      with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+          smtp.login(email_sender,email_password)
+          smtp.sendmail(email_sender,email_reciever,em.as_string())
+
+      print('succesfully sent out emails')
