380c5f00d0f75af0db1d4f680be08c082f994623
zakaria-ahmd20 <94662829+zakaria-ahmd20@users.noreply.github.com>
2022-12-31 14:17:26 -0500

    Update ADusers_creation.py

--- ADusers_creation.py
+++ ADusers_creation.py
@@ -1,45 +1,60 @@
-# Zakaria Ahmed
-from password_generator import password_generator
 import csv
 import subprocess, sys
-
+from password_generator import password_generator
+from sending import email_credentials
 def header():
-    headerList = ['Name','username','passcode','Title',]
+    headerList = ['FirstName', 'LastName', 'passcode', 'Title', 'Manager_Email']
 
     # open CSV file and assign header
-    with open("users.csv", 'w',newline='') as file:
+    with open("users.csv", 'w', newline='') as file:
         dd = csv.DictWriter(file, delimiter=',',
                             fieldnames=headerList)
         dd.writeheader()
-def add_user():
-    infile = open("users.csv", "a")
-    Name = str((input('enter your full name:')))
-    username = str((input('enter your username:')))
-    passcode = password_generator()
-    Title = str((input('enter your job title:')))
 
-    data1 = f"\n{username},{passcode},{Name},{Title}"
-    data = data1.strip() # strips white space
-    infile.write(data +"\n") # appends to new line
-    print('record added succesfully !')
-    infile.close()
-   
+def add_user():
+    while True:
+        infile = open("users.csv", "a")
+        First_name = str((input('enter the first name:')))
+        Last_name = str((input('enter the last name:')))
+        passcode = password_generator()
+        Title = str((input('enter the job title:')))
+        Manager_Email = str((input('enter the Managers email:')))
+        data1 = f"\n{First_name},{Last_name},{passcode},{Title},{Manager_Email}"
+        data = data1.strip()  # strips white space
+        infile.write(data + "\n")  # appends to new line
+        infile.close()
+        print('record added succesfully !')
+
+        choice = input('would you like to add another user Y/N')
+        if choice == 'N':
+            break
+        elif choice == 'Y':
+            continue
+        else:
+            print('"Invalid input.\nExiting."')
+            exit(1)
 def gen_user():
     p = subprocess.Popen(["powershell.exe",
-                          "C:\\Users\\Administrator\\Desktop\\import_AD_Users.ps1"],
+                          "C:\\Users\\Administrator\\Desktop\\AD_Users.ps1"],
                          stdout=sys.stdout)
-    p.communicate()
-    
+    p.communicate() # implent logging
+    choice = input('would you like to email the managers the credentials[Y]:')
+    if choice == 'Y':
+        email_credentials()
+    else:
+        print('users have been generated check the console for errors')
+
+
 def main():
     print('Welcome to AD_USERS creation')
     print('********************************************************'
-        ' **  \n'
-        ' **	Please choose one of the options below:\n'
-        ' **	[a] to add new AD user record\n'
-        ' **	[h] to initialze headers\n'
-        ' **	[G] to generate users \n'  
-        ' **	[q] to quit \n'
-        ' ********************************************************\n')
+          ' **  \n'
+          ' **	Please choose one of the options below:\n'
+          ' **	[a] to add new AD user record\n'
+          ' **	[h] to initialze headers\n'
+          ' **	[G] to generate users \n'
+          ' **	[q] to quit \n'
+          ' ********************************************************\n')
     choice = input('pick:')
     while True:
         if choice == "a":
@@ -50,9 +65,10 @@ def main():
             break
         elif choice == "G":
             gen_user()
-            break    
-        elif  choice == "q":
+            break
+        elif choice == "q":
             break
 
-        else: print('Invalid choice!')
-main()# calls all the code
+        else:
+            print('Invalid choice!')
+main()  # calls all the co
